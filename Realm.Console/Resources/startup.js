d35b8cd03fa99a14331f80e96ac4e4112fc0ee56
sebajura1234@gmail.com <sebajura1234@gmail.com>
2022-10-05 17:39:45 +0200

    lowercase symbols

--- Realm.Console/Resources/startup.js
+++ Realm.Console/Resources/startup.js
@@ -1,4 +1,4 @@
-﻿const spawnA = World.CreateSpawn("test", new Vector3(0, 2, 3));
-const spawnB = World.CreateSpawn("test", new Vector3(0, 10, 3));
+﻿const spawnA = world.createSpawn("test", new vector3(0, 2, 3));
+const spawnB = world.createSpawn("test", new vector3(0, 10, 3));
 
-Console.WriteLine("Spawns ids: {0} {1}", spawnA, spawnB.Id)
+console.writeLine("Spawns ids: {0} {1}", spawnA, spawnB.id)
--- Realm.Scripting/Runtimes/Javascript.cs
+++ Realm.Scripting/Runtimes/Javascript.cs
@@ -1,22 +1,38 @@
 ﻿using Realm.Interfaces.Scripting.Classes;
 using Realm.Scripting.Classes;
+using System.Reflection;
 
 namespace Realm.Scripting.Runtimes;
+
+class LowercaseSymbolsLoader : CustomAttributeLoader
+{
+    public override T[]? LoadCustomAttributes<T>(ICustomAttributeProvider resource, bool inherit)
+    {
+        var declaredAttributes = base.LoadCustomAttributes<T>(resource, inherit);
+        if (!declaredAttributes.Any() && typeof(T) == typeof(ScriptMemberAttribute) && resource is MemberInfo member)
+        {
+            var lowerCamelCaseName = char.ToLowerInvariant(member.Name[0]) + member.Name[1..];
+            return new[] { new ScriptMemberAttribute(lowerCamelCaseName) } as T[];
+        }
+        return declaredAttributes;
+    }
+}
 
 internal class Javascript : IScripting
 {
     private readonly V8ScriptEngine _engine;
     public Javascript(IWorld world)
     {
+        HostSettings.CustomAttributeLoader = new LowercaseSymbolsLoader();
         _engine = new V8ScriptEngine();
-        AddHostType("JavaScriptExtensions", typeof(JavaScriptExtensions));
-        AddHostType("Vector3", typeof(Vector3));
-        AddHostType("Console", typeof(Console));
+        AddHostType("javaScriptExtensions", typeof(JavaScriptExtensions));
+        AddHostType("vector3", typeof(Vector3));
+        AddHostType("console", typeof(Console));
 
-        AddHostType("World", typeof(World));
-        AddHostType("Spawn", typeof(Spawn));
+        AddHostType("world", typeof(World));
+        AddHostType("spawn", typeof(Spawn));
 
-        AddHostObject("World", world);
+        AddHostObject("world", world);
     }
 
     public void AddHostType(string name, Type type)
