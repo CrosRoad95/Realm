c2ceab35e981a347da501a9efacc0208319d6bbb
sebajura1234@gmail.com <sebajura1234@gmail.com>
2022-10-08 09:58:10 +0200

    remove wait handle from tests

--- Realm.Tests/Fixtures/ServerFixture.cs
+++ Realm.Tests/Fixtures/ServerFixture.cs
@@ -2,21 +2,15 @@
 
 public class ServerFixture : IDisposable
 {
-    private readonly EventWaitHandle waitHandle = new(false, EventResetMode.AutoReset);
     internal DefaultTestServer TestServer { get; private set; }
     public ServerFixture()
     {
         TestServer = new DefaultTestServer();
-        Task.Run(() =>
-        {
-            TestServer.TestServer.Start();
-            waitHandle.WaitOne();
-        });
+        TestServer.TestServer.Start();
     }
 
     public void Dispose()
     {
         TestServer.TestServer.Stop();
-        waitHandle.Set();
     }
 }
