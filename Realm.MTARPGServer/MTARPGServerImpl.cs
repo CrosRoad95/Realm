dd66cc9c46d80fdaf3e8d906f8758fc65a867072
sebajura1234@gmail.com <sebajura1234@gmail.com>
2022-10-25 06:36:27 +0200

    move Vector3Converter to separete file

--- Realm.MTARPGServer/MTARPGServerImpl.cs
+++ Realm.MTARPGServer/MTARPGServerImpl.cs
@@ -1,30 +1,4 @@
-﻿using Realm.Discord;
-using Realm.Interfaces.Extend;
-using Realm.Persistance;
-using Realm.Scripting;
-using Realm.Server.Scripting;
-
-namespace Realm.MTARPGServer;
-
-public class Vector3Converter : IYamlTypeConverter
-{
-    public bool Accepts(Type type) => type == typeof(Vector3);
-
-    public object? ReadYaml(IParser parser, Type type)
-    {
-        parser.Consume<SequenceStart>();
-        var x = float.Parse(parser.Consume<Scalar>().Value);
-        var y = float.Parse(parser.Consume<Scalar>().Value);
-        var z = float.Parse(parser.Consume<Scalar>().Value);
-        parser.Consume<SequenceEnd>();
-        return new Vector3(x,y,z);
-    }
-
-    public void WriteYaml(IEmitter emitter, object? value, Type type)
-    {
-        throw new NotImplementedException();
-    }
-}
+﻿namespace Realm.MTARPGServer;
 
 public class MTARPGServerImpl
 {
---
+++ Realm.MTARPGServer/Serialization/Yaml/Vector3Converter.cs
@@ -0,0 +1,21 @@
+﻿namespace Realm.MTARPGServer.Serialization.Yaml;
+
+public class Vector3Converter : IYamlTypeConverter
+{
+    public bool Accepts(Type type) => type == typeof(Vector3);
+
+    public object? ReadYaml(IParser parser, Type type)
+    {
+        parser.Consume<SequenceStart>();
+        var x = float.Parse(parser.Consume<Scalar>().Value);
+        var y = float.Parse(parser.Consume<Scalar>().Value);
+        var z = float.Parse(parser.Consume<Scalar>().Value);
+        parser.Consume<SequenceEnd>();
+        return new Vector3(x, y, z);
+    }
+
+    public void WriteYaml(IEmitter emitter, object? value, Type type)
+    {
+        throw new NotImplementedException();
+    }
+}
