5d4fa9e8c5afc1b442628cfcc5f32801e4efe980
sebajura1234@gmail.com <sebajura1234@gmail.com>
2022-11-01 06:48:41 +0100

    ignore realmrpg folder

--- RealmRPG/Server/Test/test.js
+++
@@ -1,1 +0,0 @@
-﻿export const test = "test.js"
--- RealmRPG/Server/panel.js
+++
@@ -1,7 +0,0 @@
-﻿if (moduleExists("WebPanel")) {
-    Logger.information("loading panel.js")
-
-    addEventHandler("onPlayerJoin", ({ player }) => {
-        webPanelAddSnackbar(`${player.name} wszedł na serwer`);
-    })
-}
