b6356989c58acb635b50348e0d86bee844e48e52
sebajura1234@gmail.com <sebajura1234@gmail.com>
2022-11-10 14:39:23 +0100

    remove not needed password hasher class

--- Realm.Interfaces/Server/Identity/IPasswordHasher.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Realm.Interfaces.Server.Identity;
-
-public interface IPasswordHasher
-{
-    string HashPassword(string password);
-    bool VerifyHashedPassword(string hashedPassword, string providedPassword);
-}
--- Realm.Server/Identity/PasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Realm.Server.Identity;
-
-internal class PasswordHasher : IPasswordHasher
-{
-    public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
-
-    public bool VerifyHashedPassword(string hashedPassword, string password) => BCrypt.Net.BCrypt.Verify(password, hashedPassword);
-}
