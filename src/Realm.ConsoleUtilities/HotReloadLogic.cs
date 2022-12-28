1e56cc4d6c84b0b4957c6de7680a0d0df6531ac2
sebajura1234@gmail.com <sebajura1234@gmail.com>
2022-12-28 12:13:22 +0100

    temp fix for hot reload

--- src/Realm.ConsoleUtilities/HotReloadLogic.cs
+++ src/Realm.ConsoleUtilities/HotReloadLogic.cs
@@ -8,12 +8,14 @@ namespace Realm.ConsoleUtilities;
 
 public sealed class HotReloadLogic
 {
+    private static HotReloadLogic? _hotReloadLogic;
     private readonly HotReload _hotReload;
     private readonly AgnosticGuiSystemService _agnosticGuiSystemService;
     private readonly ILogger _logger;
 
     public HotReloadLogic(AgnosticGuiSystemService agnosticGuiSystemService, ILogger logger, string path)
     {
+        _hotReloadLogic = this;
         _hotReload = new HotReload(path);
         _hotReload.OnReload += HandleHotReload;
         _agnosticGuiSystemService = agnosticGuiSystemService;
