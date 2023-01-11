8c0dc4e29c426a6414f64e38d0b8eeb7479a68f0
sebajura1234@gmail.com <sebajura1234@gmail.com>
2023-01-11 16:27:52 +0100

    add common collision shape element component, add rules to it

---
+++ src/Realm.Domain/Components/Elements/CollisionShapeElementComponent.cs
@@ -0,0 +1,47 @@
+﻿using Realm.Domain.Rules;
+
+namespace Realm.Domain.Components.Elements;
+
+public abstract class CollisionShapeElementComponent : ElementComponent
+{
+    protected readonly CollisionShape _collisionShape;
+
+    public Action<Entity>? EntityEntered { get; set; }
+    public Action<Entity>? EntityLeft { get; set; }
+
+    internal override Element Element => _collisionShape;
+
+    private readonly List<IEntityRule> _entityRules = new();
+
+    protected CollisionShapeElementComponent(CollisionShape collisionShape, Entity? createForEntity = null) : base(createForEntity)
+    {
+        _collisionShape = collisionShape;
+        _collisionShape.ElementEntered += HandleElementEntered;
+        _collisionShape.ElementLeft += HandleElementLeft;
+    }
+
+    public void AddRule(IEntityRule entityRule)
+    {
+        _entityRules.Add(entityRule);
+    }
+
+    private void HandleElementEntered(Element element)
+    {
+        if (EntityEntered != null)
+        {
+            var entity = EntityByElement.GetByElement(element);
+            if (_entityRules.All(x => x.Check(entity)))
+                EntityEntered(entity);
+        }
+    }
+
+    private void HandleElementLeft(Element element)
+    {
+        if (EntityLeft != null)
+        {
+            var entity = EntityByElement.GetByElement(element);
+            if (_entityRules.All(x => x.Check(entity)))
+                EntityLeft(entity);
+        }
+    }
+}
--- src/Realm.Domain/Components/Elements/CollisionSphereElementComponent.cs
+++ src/Realm.Domain/Components/Elements/CollisionSphereElementComponent.cs
@@ -1,30 +1,9 @@
 ﻿namespace Realm.Domain.Components.Elements;
 
-public class CollisionSphereElementComponent : ElementComponent
+public class CollisionSphereElementComponent : CollisionShapeElementComponent
 {
-    protected readonly CollisionSphere _collisionSphere;
-
-    public Action<Entity>? EntityEntered { get; set; }
-    public Action<Entity>? EntityLeft { get; set; }
-
-    internal override Element Element => _collisionSphere;
-
-    internal CollisionSphereElementComponent(CollisionSphere collisionSphere, Entity? createForEntity = null) : base(createForEntity)
+    internal CollisionSphereElementComponent(CollisionSphere collisionSphere, Entity? createForEntity = null) : base(collisionSphere, createForEntity)
     {
-        _collisionSphere = collisionSphere;
-        _collisionSphere.ElementEntered += HandleElementEntered;
-        _collisionSphere.ElementLeft += HandleElementLeft;
-    }
 
-    private void HandleElementEntered(Element element)
-    {
-        if (EntityEntered != null)
-            EntityEntered(EntityByElement.GetByElement(element));
-    }
-
-    private void HandleElementLeft(Element element)
-    {
-        if (EntityLeft != null)
-            EntityLeft(EntityByElement.GetByElement(element));
     }
 }
