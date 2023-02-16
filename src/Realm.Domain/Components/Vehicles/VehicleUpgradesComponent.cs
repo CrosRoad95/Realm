25a21f7a33a25585274b05e07c67f72bda84e421
sebajura1234@gmail.com <sebajura1234@gmail.com>
2023-02-16 20:34:16 +0100

    Improve VehicleUpgradesComponent, make it thread safe

--- src/Realm.Domain/Components/Vehicles/VehicleUpgradesComponent.cs
+++ src/Realm.Domain/Components/Vehicles/VehicleUpgradesComponent.cs
@@ -7,13 +7,15 @@ public class VehicleUpgradesComponent : Component
     [Inject]
     private VehicleUpgradeRegistry VehicleUpgradeRegistry { get; set; } = default!;
 
-    private List<int> _upgrades = new();
-    public IEnumerable<int> Upgrades => _upgrades;
+    private readonly List<int> _upgrades = new();
+    private readonly object _upgradesLock = new();
+
+    public IReadOnlyCollection<int> Upgrades => _upgrades;
 
     public VehicleUpgradesComponent()
     {
     }
-    
+
     internal VehicleUpgradesComponent(ICollection<VehicleUpgradeData> vehicleUpgrades)
     {
         _upgrades = vehicleUpgrades.Select(x => x.UpgradeId).ToList();
@@ -24,16 +26,37 @@ public class VehicleUpgradesComponent : Component
         RebuildUpgrades();
     }
 
-    public bool HasUpgrade(int upgradeId) => _upgrades.Any(x => x == upgradeId);
+    private bool InternalHasUpgrade(int upgradeId) => _upgrades.Any(x => x == upgradeId);
+
+    public bool HasUpgrade(int upgradeId)
+    {
+        lock (_upgradesLock)
+            return InternalHasUpgrade(upgradeId);
+    }
 
     public bool AddUpgrade(int upgradeId, bool rebuild = true)
     {
-        _upgrades.Add(upgradeId);
+        lock (_upgradesLock)
+            _upgrades.Add(upgradeId);
         if (rebuild)
             RebuildUpgrades();
         return true;
     }
-    
+
+    public bool AddUniqueUpgrade(int upgradeId, bool rebuild = true)
+    {
+        lock (_upgradesLock)
+        {
+            if (InternalHasUpgrade(upgradeId))
+                return false;
+        }
+        lock (_upgradesLock)
+            _upgrades.Add(upgradeId);
+        if (rebuild)
+            RebuildUpgrades();
+        return true;
+    }
+
     public bool RemoveUpgrade(int upgradeId, bool rebuild = true)
     {
         bool result = _upgrades.Remove(upgradeId);
@@ -44,23 +67,26 @@ public class VehicleUpgradesComponent : Component
 
     public void RebuildUpgrades()
     {
-        if (!_upgrades.Any())
-            return;
-
-        var upgradesEntries = _upgrades.Select(VehicleUpgradeRegistry.Get);
-        var vehicle = Entity.GetRequiredComponent<VehicleElementComponent>().Vehicle;
-        var handling = VehicleHandlingConstants.DefaultVehicleHandling[vehicle.Model];
-        foreach (var upgrade in upgradesEntries.Select(x => x.MaxVelocity).Where(x => x != null).Select(x => x!))
+        lock (_upgradesLock)
         {
-            handling.MaxVelocity += upgrade.IncreaseByUnits;
-            handling.MaxVelocity *= upgrade.MultipleBy;
-        }
-        foreach (var upgrade in upgradesEntries.Select(x => x.EngineAcceleration).Where(x => x != null).Select(x => x!))
-        {
-            handling.EngineAcceleration += upgrade.IncreaseByUnits;
-            handling.EngineAcceleration *= upgrade.MultipleBy;
-        }
+            if (!_upgrades.Any())
+                return;
 
-        vehicle.Handling = handling;
+            var upgradesEntries = _upgrades.Select(VehicleUpgradeRegistry.Get);
+            var vehicle = Entity.GetRequiredComponent<VehicleElementComponent>().Vehicle;
+            var handling = VehicleHandlingConstants.DefaultVehicleHandling[vehicle.Model];
+            foreach (var upgrade in upgradesEntries.Select(x => x.MaxVelocity).Where(x => x != null).Select(x => x!))
+            {
+                handling.MaxVelocity += upgrade.IncreaseByUnits;
+                handling.MaxVelocity *= upgrade.MultipleBy;
+            }
+            foreach (var upgrade in upgradesEntries.Select(x => x.EngineAcceleration).Where(x => x != null).Select(x => x!))
+            {
+                handling.EngineAcceleration += upgrade.IncreaseByUnits;
+                handling.EngineAcceleration *= upgrade.MultipleBy;
+            }
+
+            vehicle.Handling = handling;
+        }
     }
 }
