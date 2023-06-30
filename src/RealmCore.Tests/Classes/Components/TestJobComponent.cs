f3a3cddb9dfe08c434ebdce4c429d38ee00d2c80
Sebastian Jura <sebajura1234@gmail.com>
2023-06-30 20:53:26 +0200

    add JobSessionComponentTests

---
+++ src/RealmCore.Tests/Classes/Components/TestJobComponent.cs
@@ -0,0 +1,51 @@
+﻿using RealmCore.Server.Components.Object;
+using RealmCore.Server.Components.Players.Jobs;
+using RealmCore.Server.Concepts.Objectives;
+using SlipeServer.Server.Elements;
+using SlipeServer.Server.Services;
+using System.Numerics;
+
+namespace RealmCore.Tests.Classes.Components;
+
+internal class TestJobComponent : JobSessionComponent
+{
+    public override short JobId => 1;
+
+    public TestJobComponent()
+    {
+
+    }
+
+    public void CreateObjectives()
+    {
+        var objective = AddObjective(new MarkerEnterObjective(new Vector3(383.6543f, -82.01953f, 3.914598f)));
+        objective.AddBlip(BlipIcon.North, EntityFactory);
+        objective.Completed += ObjectiveACompleted;
+
+        var objectEntity = EntityFactory.CreateObject(SlipeServer.Server.Enums.ObjectModel.Gunbox, new Vector3(379.00f, -102.77f, 1.24f), Vector3.Zero);
+        objectEntity.AddComponent<LiftableWorldObjectComponent>();
+        var objective2 = AddObjective(new TransportEntityObjective(objectEntity, new Vector3(379.00f, -112.77f, 2.0f)));
+        objective2.Completed += ObjectiveBCompleted;
+
+        var subObjective1 = new MarkerEnterObjective(new Vector3(386.9004f, -89.74414f, 3.8843315f));
+        var subObjective2 = new MarkerEnterObjective(new Vector3(393.80566f, -99.60156f, 5.2993988f));
+        var objective1 = AddObjective(new OneOfObjective(subObjective1, subObjective2));
+        objective1.Completed += ObjectiveCCompleted;
+    }
+
+    private void ObjectiveACompleted(Objective objective)
+    {
+        Entity.GetRequiredComponent<JobStatisticsComponent>().AddPoints(1, 1);
+    }
+
+    private void ObjectiveBCompleted(Objective objective)
+    {
+        Entity.GetRequiredComponent<JobStatisticsComponent>().AddPoints(1, 2);
+    }
+
+    private void ObjectiveCCompleted(Objective objective)
+    {
+        Entity.GetRequiredComponent<JobStatisticsComponent>().AddPoints(1, 1);
+    }
+
+}
---
+++ src/RealmCore.Tests/Tests/Components/JobSessionComponentTests.cs
@@ -0,0 +1,51 @@
+﻿using FluentAssertions.Common;
+using RealmCore.Resources.GuiSystem;
+using RealmCore.Server;
+using RealmCore.Server.Components.Elements;
+using RealmCore.Server.Components.Elements.CollisionShapes;
+using RealmCore.Server.Factories;
+using RealmCore.Tests.Classes.Components;
+using SlipeServer.Server.ElementCollections;
+using SlipeServer.Server.Elements;
+
+namespace RealmCore.Tests.Tests.Components;
+
+public class JobSessionComponentTests
+{
+    private readonly IElementCollection _elementCollection;
+    private readonly EntityHelper _entityHelper;
+    private readonly Mock<IRPGServer> _rpgServerMock = new(MockBehavior.Strict);
+    private readonly RealmTestingServer _realmTestingServer;
+    private readonly Mock<IGuiSystemService> _guiSystemService = new(MockBehavior.Strict);
+
+    public JobSessionComponentTests()
+    {
+        _rpgServerMock.Setup(x => x.IsReady).Returns(true);
+        _rpgServerMock.Setup(x => x.AssociateElement(It.IsAny<object>())).Callback<object>(e =>
+        {
+            _elementCollection.Add((Element)e);
+        });
+        _realmTestingServer = new(null, services =>
+        {
+            services.AddSingleton<IEntityFactory, EntityFactory>();
+            services.AddSingleton(_guiSystemService.Object);
+            services.AddSingleton(_rpgServerMock.Object);
+        });
+        _entityHelper = new(_realmTestingServer);
+        _elementCollection = _realmTestingServer.GetRequiredService<IElementCollection>();
+    }
+
+    [Fact]
+    public void JobShouldCreateExpectedAmountOfPrivateAndPublicElements()
+    {
+        var playerEntity = _entityHelper.CreatePlayerEntity();
+        var testJobComponent = playerEntity.AddComponent<TestJobComponent>();
+        playerEntity.AddComponent(new JobStatisticsComponent(DateTime.Now));
+        testJobComponent.CreateObjectives();
+        var elements = _elementCollection.GetAll().ToList();
+        var components =  playerEntity.Components.ToList();
+        components.OfType<PlayerPrivateElementComponent<MarkerElementComponent>>().Should().HaveCount(4);
+        components.OfType<PlayerPrivateElementComponent<CollisionSphereElementComponent>>().Should().HaveCount(4);
+        components.OfType<PlayerPrivateElementComponent<BlipElementComponent>>().Should().HaveCount(1);
+    }
+}
