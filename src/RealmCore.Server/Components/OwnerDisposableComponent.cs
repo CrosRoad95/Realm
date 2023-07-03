e15f1746bfab591110814e8b8748a50b8f82d8b9
Sebastian Jura <sebajura1234@gmail.com>
2023-07-03 06:54:47 +0200

    add OwnerDisposableComponent

---
+++ src/RealmCore.Server/Components/OwnerDisposableComponent.cs
@@ -0,0 +1,22 @@
+﻿namespace RealmCore.Server.Components;
+
+public class OwnerDisposableComponent : Component
+{
+    public Entity OwningEntity { get; }
+
+    public OwnerDisposableComponent(Entity owningEntity)
+    {
+        OwningEntity = owningEntity;
+        OwningEntity.Disposed += HandleDisposed;
+    }
+
+    private void HandleDisposed(Entity entity)
+    {
+        Entity.Dispose();
+    }
+
+    public override void Dispose()
+    {
+        OwningEntity.Disposed -= HandleDisposed;
+    }
+}
