820aca6453a8abb895255993b5b799a3044098f4
Sebastian Jura <sebajura1234@gmail.com>
2023-07-13 17:31:28 +0200

    add RealmInternal to get elements from components

---
+++ src/RealmCore.Server/RealmInternal.cs
@@ -0,0 +1,7 @@
+﻿namespace RealmCore.Server;
+
+public static class RealmInternal
+{
+    public static Element GetElement(ElementComponent elementComponent) => elementComponent.Element;
+    public static Player GetPlayer(ElementComponent elementComponent) => (Player)elementComponent.Element;
+}
