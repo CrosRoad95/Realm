da2d401bc38d9dd8bd51d764baf10fe7ecf5c1b5
Sebastian Jura <sebajura1234@gmail.com>
2024-02-20 06:29:41 +0100

    Add offset to "CreateFor" extension methof for Text3dService

--- src/RealmCore.Server/Extensions/Text3dServiceExtensions.cs
+++ src/RealmCore.Server/Extensions/Text3dServiceExtensions.cs
@@ -2,8 +2,8 @@
 
 public static class Text3dServiceExtensions
 {
-    public static int CreateFor(this Text3dService service, Element element, string text, float fontSize = 1f, float distance = 64f, Color? color = null)
+    public static int CreateFor(this Text3dService service, Element element, string text, float fontSize = 1f, float distance = 64f, Color? color = null, Vector3 offset = default)
     {
-        return service.CreateText3d(element.Position, text, fontSize, distance, color);
+        return service.CreateText3d(element.Position + offset, text, fontSize, distance, color);
     }
 }
