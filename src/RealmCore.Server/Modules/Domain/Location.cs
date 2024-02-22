cb2ce7546a174ca51815764b23283a1b87f9821f
Sebastian Jura <sebajura1234@gmail.com>
2024-02-22 16:33:58 +0100

    Add ToString overload for Location

--- src/RealmCore.Server/Modules/Domain/Location.cs
+++ src/RealmCore.Server/Modules/Domain/Location.cs
@@ -7,6 +7,20 @@ public record Location(Vector3 Position, Vector3 Rotation = default, byte? Inter
     public Location(float x, float y, float z) : this(new Vector3(x, y, z), default, null, null) { }
     public Location() : this(default, default, null, null) { }
 
-    private static Location _location = new();
+    private static readonly Location _location = new();
     public static Location Zero => _location;
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        sb.Append($"Position: {Position.X:0.00}, {Position.Y:0.00}, {Position.Z:0.00}");
+        if (Rotation != Vector3.Zero)
+            sb.Append($", Rotation: {Rotation.X:0.00}, {Rotation.Y:0.00}, {Rotation.Z:0.00}");
+        if (Interior != null)
+            sb.Append($", Interior: {Interior}");
+        if (Interior != null)
+            sb.Append($", Dimension: {Dimension}");
+
+        return sb.ToString();
+    }
 }
--- src/RealmCore.Tests/Unit/Domain/LocationTests.cs
+++ src/RealmCore.Tests/Unit/Domain/LocationTests.cs
@@ -10,5 +10,7 @@ public class LocationTests
         var location2 = location with { Dimension = 3 };
 
         location2.Should().BeEquivalentTo(new Location(new Vector3(1, 2, 3), new Vector3(1, 2, 3), 1, 3));
+
+        location2.ToString().Should().Be("Position: 1.00, 2.00, 3.00, Rotation: 1.00, 2.00, 3.00, Interior: 1, Dimension: 3");
     }
 }
