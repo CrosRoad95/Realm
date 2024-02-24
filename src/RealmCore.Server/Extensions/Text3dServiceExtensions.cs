9aabc2e2c6f1255bb611e974ed8e638c4500193a
Sebastian Jura <sebajura1234@gmail.com>
2024-02-24 08:40:58 +0100

    Update SlipeResources, add interior and dimension for Text3dServiceExtensions CreateFor method

--- src/RealmCore.Server/Extensions/Text3dServiceExtensions.cs
+++ src/RealmCore.Server/Extensions/Text3dServiceExtensions.cs
@@ -2,8 +2,8 @@
 
 public static class Text3dServiceExtensions
 {
-    public static int CreateFor(this Text3dService service, Element element, string text, float fontSize = 1f, float distance = 64f, Color? color = null, Vector3 offset = default)
+    public static int CreateFor(this Text3dService service, Element element, string text, float fontSize = 1f, float distance = 64f, Color? color = null, byte interior = 0, ushort dimension = 0, Vector3 offset = default)
     {
-        return service.CreateText3d(element.Position + offset, text, fontSize, distance, color);
+        return service.CreateText3d(element.Position + offset, text, fontSize, distance, color, interior, dimension);
     }
 }
