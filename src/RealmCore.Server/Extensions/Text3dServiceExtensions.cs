5232250b145305f88dfa9da32b4beebf4ec238da
Sebastian Jura <sebajura1234@gmail.com>
2024-04-01 14:42:48 +0200

    Clear text3d when element gets destroyed

--- src/RealmCore.Server/Extensions/Text3dServiceExtensions.cs
+++ src/RealmCore.Server/Extensions/Text3dServiceExtensions.cs
@@ -2,8 +2,14 @@
 
 public static class Text3dServiceExtensions
 {
-    public static int CreateFor(this Text3dService service, Element element, string text, float fontSize = 1f, float distance = 64f, Color? color = null, byte interior = 0, ushort dimension = 0, Vector3 offset = default)
+    public static int CreateFor(this Text3dService text3dService, Element element, string text, float fontSize = 1f, float distance = 64f, Color? color = null, byte interior = 0, ushort dimension = 0, Vector3 offset = default)
     {
-        return service.CreateText3d(element.Position + offset, text, fontSize, distance, color, interior, dimension);
+        var text3dId = text3dService.CreateText3d(element.Position + offset, text, fontSize, distance, color, interior, dimension);
+        element.Destroyed += (e) =>
+        {
+            text3dService.RemoveText3d(text3dId);
+        };
+
+        return text3dId;
     }
 }
