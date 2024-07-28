f7e54b278c1e8ffb7ab7018d4404644dc08bff09
Sebastian Jura <22455534+CrosRoad95@users.noreply.github.com>
2024-07-28 09:30:51 +0200

    Fix Text3dServiceExtensions.CreateFor

--- src/RealmCore.Server/Extensions/Text3dServiceExtensions.cs
+++ src/RealmCore.Server/Extensions/Text3dServiceExtensions.cs
@@ -2,9 +2,9 @@
 
 public static class Text3dServiceExtensions
 {
-    public static int CreateFor(this Text3dService text3dService, Element element, string text, float fontSize = 1f, float distance = 64f, Color? color = null, byte interior = 0, ushort dimension = 0, Vector3 offset = default, Vector2? shadow = null)
+    public static int CreateFor(this Text3dService text3dService, Element element, string text, float fontSize = 1f, float distance = 64f, Color? color = null, Vector3 offset = default, Vector2? shadow = null)
     {
-        var text3dId = text3dService.CreateText3d(element.Position + offset, text, fontSize, distance, color, interior, dimension, shadow);
+        var text3dId = text3dService.CreateText3d(element.Position + offset, text, fontSize, distance, color, element.Interior, element.Dimension, shadow);
         element.Destroyed += (e) =>
         {
             text3dService.RemoveText3d(text3dId);
