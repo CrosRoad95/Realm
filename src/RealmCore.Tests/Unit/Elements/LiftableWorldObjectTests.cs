f19600f4d8ca4a8b41a05ad3dc5fb8fe1c1115b4
Sebastian Jura <22455534+CrosRoad95@users.noreply.github.com>
2024-08-29 21:15:42 +0200

    Update LiftableWorldObjectTests

--- src/RealmCore.Tests/Unit/Elements/LiftableWorldObjectTests.cs
+++ src/RealmCore.Tests/Unit/Elements/LiftableWorldObjectTests.cs
@@ -1,138 +1,119 @@
 ﻿namespace RealmCore.Tests.Unit.Elements;
 
-public class LiftableWorldObjectTests
+public class LiftableWorldObjectTests : IClassFixture<RealmTestingServerHostingFixtureWithPlayer>
 {
-    [Fact]
-    public async Task YouShouldBeAbleToLiftElementAndDropElement()
+    private readonly RealmTestingServerHostingFixtureWithPlayer _fixture;
+    private readonly RealmTestingServerHosting<RealmTestingPlayer> _hosting;
+    private readonly RealmTestingPlayer _player;
+
+    public LiftableWorldObjectTests(RealmTestingServerHostingFixtureWithPlayer fixture)
     {
-        using var hosting = new RealmTestingServerHosting();
-        var player = await hosting.CreatePlayer();
-        var worldObject = hosting.CreateObject();
+        _fixture = fixture;
+        _player = _fixture.Player;
+        _hosting = fixture.Hosting;
+    }
+
+    [Fact]
+    public void YouShouldBeAbleToLiftElementAndDropElement()
+    {
+        var worldObject = _hosting.CreateObject();
 
         var interaction = new LiftableInteraction();
         worldObject.Interaction = new LiftableInteraction();
 
         using var monitored = interaction.Monitor();
-        var result1 = interaction.TryLift(player);
+        var result1 = interaction.TryLift(_player);
         var wasOwner = interaction.Owner;
         var result2 = interaction.TryDrop();
 
+        using var _ = new AssertionScope();
         result1.Should().BeTrue();
         result2.Should().BeTrue();
-        wasOwner.Should().Be(player);
+        wasOwner.Should().Be(_player);
         monitored.Should().Raise(nameof(LiftableInteraction.Lifted));
         monitored.Should().Raise(nameof(LiftableInteraction.Dropped));
     }
 
     [Fact]
-    public async Task ElementCanBeLiftedOnce()
+    public void ElementCanBeLiftedOnce()
     {
-        using var hosting = new RealmTestingServerHosting();
-        var player = await hosting.CreatePlayer();
-        var worldObject = hosting.CreateObject();
+        var worldObject = _hosting.CreateObject();
 
         var interaction = new LiftableInteraction();
         worldObject.Interaction = new LiftableInteraction();
 
-        interaction.TryLift(player);
-        var result = interaction.TryLift(player);
+        interaction.TryLift(_player);
+        var result = interaction.TryLift(_player);
 
         result.Should().BeFalse();
     }
 
     [Fact]
-    public async Task ElementCanBeDroppedOnce()
+    public void ElementCanBeDroppedOnce()
     {
-        using var hosting = new RealmTestingServerHosting();
-        var player = await hosting.CreatePlayer();
-        var worldObject = hosting.CreateObject();
+        var worldObject = _hosting.CreateObject();
         var interaction = new LiftableInteraction();
         worldObject.Interaction = new LiftableInteraction();
 
-        #region Act
-        interaction.TryLift(player);
+        interaction.TryLift(_player);
         interaction.TryDrop();
         var result = interaction.TryDrop();
-        #endregion
 
-        #region Assert
         result.Should().BeFalse();
-        #endregion
     }
 
     [Fact]
     public async Task ElementShouldBeDroppedUponDispose()
     {
-        using var hosting = new RealmTestingServerHosting();
-        var player = await hosting.CreatePlayer();
-        var worldObject = hosting.CreateObject();
+        var worldObject = _hosting.CreateObject();
         var interaction = new LiftableInteraction();
         worldObject.Interaction = new LiftableInteraction();
 
-        #region Arrange
         using var monitored = interaction.Monitor();
-        interaction.TryLift(player);
-        #endregion
+        interaction.TryLift(_player);
 
-        #region Act
-        player.Destroy();
-        #endregion
+        await _hosting.DisconnectPlayer(_player);
 
-        #region Assert
+        using var _ = new AssertionScope();
         interaction.Owner.Should().BeNull();
         monitored.Should().Raise(nameof(interaction.Lifted));
         monitored.Should().Raise(nameof(interaction.Dropped));
-        #endregion
     }
 
     [Fact]
     public async Task ElementShouldBeDroppedUponDispose2()
     {
-        using var hosting = new RealmTestingServerHosting();
-        var player = await hosting.CreatePlayer();
-        var worldObject = hosting.CreateObject();
+        var worldObject = _hosting.CreateObject();
         var interaction = new LiftableInteraction();
         worldObject.Interaction = new LiftableInteraction();
 
-        #region Arrange
         using var monitored = interaction.Monitor();
-        interaction.TryLift(player);
-        #endregion
+        interaction.TryLift(_player);
 
-        #region Act
-        player.Destroy();
-        #endregion
+        await _hosting.DisconnectPlayer(_player);
 
-        #region Assert
+        using var _ = new AssertionScope();
         interaction.Owner.Should().BeNull();
         monitored.Should().Raise(nameof(interaction.Lifted));
         monitored.Should().Raise(nameof(interaction.Dropped));
-        #endregion
     }
 
     [Fact]
     public async Task OnlyWhitelistedEntitiesShouldBeAbleToLiftOtherElement()
     {
-        #region Arrange
-        using var hosting = new RealmTestingServerHosting();
-        var player1 = await hosting.CreatePlayer();
-        var player2 = await hosting.CreatePlayer();
-        var worldObject = hosting.CreateObject();
+        var player2 = await _hosting.CreatePlayer();
+        var worldObject = _hosting.CreateObject();
 
         var interaction1 = new LiftableInteraction();
-        var interaction2 = new LiftableInteraction(player1);
-        #endregion
+        var interaction2 = new LiftableInteraction(_player);
 
-        #region Act
-        bool lifted1 = interaction1.TryLift(player1);
+        bool lifted1 = interaction1.TryLift(_player);
         bool dropped1 = interaction1.TryDrop();
         bool lifted2 = interaction2.TryLift(player2);
-        #endregion
 
-        #region Assert
+        using var _ = new AssertionScope();
         lifted1.Should().BeTrue();
         dropped1.Should().BeTrue();
         lifted2.Should().BeFalse();
-        #endregion
     }
 }
